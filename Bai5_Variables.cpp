4d0853ec5d7626ff7f52b91c627498b5a1e4527d
Huy221107 <106250073@sv1.dut.udn.vn>
2026-08-11 00:05:25 +0700

    Add Bai5_Variables.cpp

---
+++ Bai5_Variables.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include<string>
+using namespace std;
+int main(){
+    int tuoi=19;
+    int namsinh=2007;
+    float chieucao=1.65;
+    double cannang=52.5, luongthang=5000000;
+    char xeploai ='A';
+    string ten= "Nguyen Chi Huy";
+    bool trangthaiquamon=true;
+    cout<<"Ten :"<<ten<<endl;
+    cout<<"Tuoi :"<< tuoi<<endl;
+    cout<<"Nam sinh :"<< namsinh<<endl;
+    cout<<"Chieu cao :"<< chieucao<<"m"<<endl;
+    cout<<"Can nang :"<< cannang<<"Kg"<<endl;
+    cout<<"Xep loai :"<<xeploai<<endl;
+    cout<< "Trang thai qua mon :"<< trangthaiquamon<<endl;
+    cout<< "Luong thang :"<< luongthang<<endl;
+    return 0;
+}
