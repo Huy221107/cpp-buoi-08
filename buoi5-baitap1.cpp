308b3b4bb19d75137aced6b5e9c23128e76a6e69
Huy221107 <106250073@sv1.dut.udn.vn>
2026-08-11 01:39:33 +0700

    Keep only baitap1b6 and baitap2b6

--- buoi5-baitap1.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;    
-int main(){
-    int a=5,b=10,c=15,tong;
- tong=a+b+c;
- cout<<" tong gia tri cua a,b,c la :"<<tong<<endl;
- cout<< " gia tri trung binh cua a,b,c la :"<<float(tong)/3<<endl;
- return 0;
-}
