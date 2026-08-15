0d924a27405a2b568623644d8e0b941007101e08
Huy221107 <106250073@sv1.dut.udn.vn>
2026-08-15 12:22:03 +0700

    Add baitap3b8.cpp - find smallest divisor and check prime

---
+++ baitap3b8.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<< "nhap vao so nguyen duong n (n>=2) :";
+    cin>>n;
+    if(n>=2){
+        for(int i=2;i<n;i++){
+            if(n%i==0){
+                cout<<"uoc nho nhat lon hon 1 la :"<<i <<endl;
+                break;
+            }
+
+        }cout<<"n la so nguyen to ";
+    }
+    else{
+        cout<< " khong hop le";
+    }
+}
